09d1134e41d6cabd223c9dc36227b2144d655f5a
Neal Robison <nealjrobison@gmail.com>
2025-11-30 11:00:51 -0500

    Change setting change toasts from slow to fast.

--- TravelTriggers/Command/CommandManager.cs
+++ TravelTriggers/Command/CommandManager.cs
@@ -49,7 +49,7 @@ namespace TravelTriggers.Command
                     {
                         config.PluginEnabled = !config.PluginEnabled;
                         TravelTriggers.PluginConfiguration.Save();
-                        TravelTriggers.Commands.ProcessCommand($"/popup -n -s TravelTriggers Plugin {(config.PluginEnabled ? "Enabled" : "Disabled")}");
+                        TravelTriggers.Commands.ProcessCommand($"/popup -n -f TravelTriggers Plugin {(config.PluginEnabled ? "Enabled" : "Disabled")}");
                     }
                     break;
                 case SettingsCommand when args == "rp":
@@ -57,7 +57,7 @@ namespace TravelTriggers.Command
                     {
                         config.RoleplayOnly = !config.RoleplayOnly;
                         TravelTriggers.PluginConfiguration.Save();
-                        TravelTriggers.Commands.ProcessCommand($"/popup -n -s TravelTriggers Roleplay Only Module {(config.RoleplayOnly ? "Enabled" : "Disabled")}");
+                        TravelTriggers.Commands.ProcessCommand($"/popup -n -f TravelTriggers Roleplay Only Module {(config.RoleplayOnly ? "Enabled" : "Disabled")}");
                     }
                     break;
 
@@ -66,7 +66,7 @@ namespace TravelTriggers.Command
                     {
                         config.EnableRNG = !config.EnableRNG;
                         TravelTriggers.PluginConfiguration.Save();
-                        TravelTriggers.Commands.ProcessCommand($"/popup -n -s TravelTriggers RNG Module {(config.EnableRNG ? "Enabled" : "Disabled")}");
+                        TravelTriggers.Commands.ProcessCommand($"/popup -n -f TravelTriggers RNG Module {(config.EnableRNG ? "Enabled" : "Disabled")}");
                     }
                     break;
                 case SettingsCommand when args == "gs":
@@ -74,7 +74,7 @@ namespace TravelTriggers.Command
                     {
                         config.EnableGearsetSwap = !config.EnableGearsetSwap;
                         TravelTriggers.PluginConfiguration.Save();
-                        TravelTriggers.Commands.ProcessCommand($"/popup -n -s TravelTriggers Gearset Module {(config.EnableGearsetSwap ? "Enabled" : "Disabled")}");
+                        TravelTriggers.Commands.ProcessCommand($"/popup -n -f TravelTriggers Gearset Module {(config.EnableGearsetSwap ? "Enabled" : "Disabled")}");
                     }
                     break;
                 case SettingsCommand when args?.Length == 0:
